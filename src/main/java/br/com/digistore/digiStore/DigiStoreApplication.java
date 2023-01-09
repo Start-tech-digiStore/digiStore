dcddae865cab4307609fac3ac6c62311579b4df7
Sérgio Augusto David Jr <sergio.adjr@gmail.com>
2023-01-09 13:00:17 -0400

    first commit

---
+++ src/main/java/br/com/digistore/digiStore/DigiStoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.digistore.digiStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigiStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/digistore/digiStore/DigiStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.digistore.digiStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
